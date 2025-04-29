9b45322d8151c53a847dc858d91e94afa953b6ed
yqdvan <yqdvan@github.com>
2025-04-29 21:29:51 +0800

    fix path

--- start_in_linux.sh
+++ start_in_linux.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# Author : Duan Yuanqiang
-# Version: v0.1
-#          Initial version
 
-TOOLDIR=`dirname $0 | sed -e "s/^\(.*\/arsoc\/\).*$/\1/"` 
- echo ${TOOLDIR}
+TOOLDIR=“./”
 
 module unload python3
 module load python/3.8.8
